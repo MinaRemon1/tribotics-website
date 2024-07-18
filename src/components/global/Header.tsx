dca065b812135e08b74577f98b0ab63264f0c42f
Your Name <mina.remon@icloud.com>
2024-07-18 17:08:59 +0300

    commit

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -35,12 +35,19 @@ const Header = () => {
             <nav className={`fixed w-full h-24 right-0 left-0 top-0 pl-3 pr-3 md:pl-10 md:pr-10 py-4 px-4 z-[100] flex items-center border-neutral-900 justify-between transition-colors duration-500 ${scrolled ? 'backdrop-blur-lg bg-black/65' : 'bg-transparent'}`}>
                 <div className='flex items-center justify-between h-full w-full'>
                     <Link href='/'>
-                        <Image 
+                        {/* <Image 
                             src="/logo2.png"
                             width={150}
                             height={70}
                             alt="Logo"
                             className="cursor-pointer"
+                        /> */}
+                        <Image 
+                            src="/logo2.png"
+                            width={150}
+                            height={70}
+                            alt="Logo"
+                            className="cursor-pointer w-24 h-16 md:w-40 md:h-24"
                         />
                     </Link>
                     <div className='text-white hidden md:flex sm:flex'>
