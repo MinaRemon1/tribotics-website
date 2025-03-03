3ee26a68761c6e2671fd3a66e9917b35767f94c5
Your Name <mina.remon@icloud.com>
2025-03-03 20:57:59 +0200

    commit

--- src/slices/VideoBlock/index.tsx
+++ src/slices/VideoBlock/index.tsx
@@ -10,22 +10,22 @@ export type VideoBlockProps = SliceComponentProps<Content.VideoBlockSlice>;
  * Component for "VideoBlock" Slices.
  */
 const VideoBlock = ({ slice }: VideoBlockProps): JSX.Element => {
-
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className=""
     >
-      <div className="max-w-prose rounded-lg">
-        <iframe
-              width={700}
-              height={415}
-              className='aspect-[16/9] my-10 rounded-xl'
-              src={`https://www.youtube-nocookie.com/embed/${slice.primary.youtube_id}`}
-              allow='accelerometer; autoplay; clipboard-write; encrypt-media; gyroscope; picture-in-picture'
-              allowFullScreen
-              title='Youtube Video'
+      <div className="w-full sm:w-[100%] lg:max-w-2xl mx-auto sm:mx-0">
+        <div className="relative w-full aspect-video my-10 rounded-xl overflow-hidden">
+          <iframe
+            className="absolute top-0 left-0 w-full h-full rounded-xl"
+            src={`https://www.youtube-nocookie.com/embed/${slice.primary.youtube_id}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="YouTube Video"
           />
+        </div>
       </div>
     </section>
   );
